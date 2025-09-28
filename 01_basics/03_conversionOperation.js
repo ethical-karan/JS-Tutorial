c1ee08642aaf1752643249ee246ee1e4c0dcbbf5
ethical-karan <165396896+ethical-karan@users.noreply.github.com>
2025-09-28 06:10:56 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "34" => 33       (easily convert into number)
+// "43abc" => Nan   (this is also num, but this is nan)
+// true => 1; false => 0
+
+// let isLoggedIn =3
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//  any value conveted into string
+
+let someNumber = 3
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
